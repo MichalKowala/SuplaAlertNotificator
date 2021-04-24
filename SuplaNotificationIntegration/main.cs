c4ebdc99f64f4846cf02bf88c78f114ef5e18eca
truemarin <65551447+truemarin@users.noreply.github.com>
2021-04-24 22:11:35 +0200

    Init

---
+++ SuplaNotificationIntegration/main.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+
+namespace SuplaNotificationIntegration
+{
+    public static class Function
+    {
+        [FunctionName("Executor")]
+        public static void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+        {
+            Console.WriteLine("Test123");
+        }
+    }
+}
