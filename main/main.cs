557fbeb284945815a2adf504618fc2d0f19e7a73
Michał Kowala <michalkowala111@gmail.com>
2021-04-26 17:00:29 +0200

    add reading container files

--- main/main.cs
+++ main/main.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace SuplaNotificationIntegration
 {
     public static class Function
     {
         [FunctionName("Executor")]
-        public static void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+        public static async Task RunAsync([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
         {
-            Console.WriteLine("Test123");
+            string connectionString = Environment.GetEnvironmentVariable("SNISTORAGEACCOUNT");
+            var account = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient _client = account.CreateCloudBlobClient();
+            CloudBlobContainer _container = _client.GetContainerReference("sni-container");
+            var subscribers = await _container.GetBlockBlobReference("subscribers.json").DownloadTextAsync();
+            var sensors = await _container.GetBlockBlobReference("sensors.json").DownloadTextAsync();
         }
     }
 }
