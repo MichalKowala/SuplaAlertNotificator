456e1a966fddcbaea6913d5ec96ae8faff9fd1db
Michał Kowala <michalkowala111@gmail.com>
2021-04-28 21:03:51 +0200

    Reports can now be mailed

--- main/ReportManager.cs
+++ main/ReportManager.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace SuplaNotificationIntegration
 {
@@ -36,6 +39,25 @@ namespace SuplaNotificationIntegration
                 return message.ToString();
             else
                 return "ok";
+        }
+        public async Task MailTheReport(string message)
+        {
+            message = "ALERT Kto gra w dotkę?";
+            var apiKey = Environment.GetEnvironmentVariable("test_klucz");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("");
+            var to = new EmailAddress("");
+            var subject = $"Report {DateTime.Now}";
+            var plainTextContent = message;
+            var htmlContent = "<strong> test siemaneczko </strong>";
+            var msg = MailHelper.CreateSingleEmail(
+                from,
+                to,
+                subject,
+                plainTextContent,
+                plainTextContent);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine();
         }
     }
 }
