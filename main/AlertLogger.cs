bf7340f897e6a4d72fb2e30e13662142bbbdac24
Michał Kowala <michalkowala111@gmail.com>
2021-04-30 18:04:41 +0200

    Add StorageAccessHelper

--- main/AlertLogger.cs
+++ main/AlertLogger.cs
@@ -1,12 +1,7 @@
-﻿using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
-using Microsoft.Azure.Storage;
+﻿using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace SuplaNotificationIntegration
 {
@@ -15,10 +10,8 @@ namespace SuplaNotificationIntegration
         public void Log(string message)
         {
             string fileName = Environment.GetEnvironmentVariable(EnvKeys.AlertLogsFolder) + "/" + DateTime.Now.ToString("dd.MM.yyyy");
-            CloudStorageAccount storage = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable(EnvKeys.StorageConnString));
-            CloudBlobClient client = storage.CreateCloudBlobClient();
-            CloudBlobContainer container = client.GetContainerReference(Environment.GetEnvironmentVariable(EnvKeys.SniContainer));
-            CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+            StorageAccessHelper storage = new StorageAccessHelper();
+            CloudBlockBlob blob = storage.GetBlockBlobReference(fileName);
             using (MemoryStream stream = new MemoryStream())
             {
                 if (blob.Exists())
---
+++ main/StorageAccessHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuplaNotificationIntegration
+{
+    public class StorageAccessHelper
+    {
+        private readonly CloudStorageAccount _storage;
+        private readonly CloudBlobClient _client;
+        private readonly CloudBlobContainer _container;
+        public StorageAccessHelper()
+        {
+            _storage = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable(EnvKeys.StorageConnString));
+            _client = _storage.CreateCloudBlobClient();
+            _container = _client.GetContainerReference(Environment.GetEnvironmentVariable(EnvKeys.SniContainer));
+        }
+        public CloudBlockBlob GetBlockBlobReference(string filename)
+        {
+            return _container.GetBlockBlobReference(filename);
+        }
+        
+    }
+}
