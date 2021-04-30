68580d25fff18b0f4442addcb40abc66b65444d6
Michał Kowala <michalkowala111@gmail.com>
2021-04-30 14:12:12 +0200

    Add EnvKeys class

---
+++ main/EnvKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuplaNotificationIntegration
+{
+    public static class EnvKeys
+    {
+        public static string StorageConnString => "SNI_STORAGE_ACC_CONNECTION_STRING";
+        public static string SniContainer => "SNI_CONTAINER";
+        public static string DevicesFile => "SNI_DEVICES_FILENAME";
+        public static string SubscribersFile => "SNI_SUBSCRIBERS_FILENAME";
+    }
+}
