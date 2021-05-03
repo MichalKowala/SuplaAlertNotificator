183aeefd8ac77a3fefd51ad534b9eb1d4e55a442
Michał Kowala <michalkowala111@gmail.com>
2021-05-03 21:11:03 +0200

    Add Startup

---
+++ main/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Cosmonaut;
+using Cosmonaut.Extensions.Microsoft.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using SNIClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: FunctionsStartup(typeof(SuplaNotificationIntegration.Startup))]
+namespace SuplaNotificationIntegration
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+          
+        }
+    }
+}
